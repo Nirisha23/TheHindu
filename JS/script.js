5e093c7fc9bf8e2d3c15e3868affaa8b2e93eb43
Nirisha <nirisha.n@outlook.com>
2024-03-09 12:18:53 +0530

    Commit 2 removed unwanted codes

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const menuIcon = document.getElementById('menuIcon');
-//     const loginBtn = document.getElementById('loginBtn');
-//     const subscribeBtn = document.getElementById('subscribeBtn');
-
-//     menuIcon.addEventListener('click', function () {
-//         if (loginBtn.style.display === 'none' && subscribeBtn.style.display === 'none') {
-//             loginBtn.style.display = 'inline-block';
-//             subscribeBtn.style.display = 'inline-block';
-//             menuIcon.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
-//         } else {
-//             loginBtn.style.display = 'none';
-//             subscribeBtn.style.display = 'none';
-//             menuIcon.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
-//         }
-//     });
-// });
-
-
 hamburger = document.querySelector('#menuIcon');
 navbar = document.querySelector('#navLinksContainer');
 
