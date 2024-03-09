293bc1a92bb1def6724d135e4cd00e1cf0b4d9c0
Nirisha <nirisha.n@outlook.com>
2024-03-09 12:16:18 +0530

    Commit 1

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("menuIcon").addEventListener("click", function () {
-        console.log("Menu icon clicked");
-        var navAuth = document.querySelector(".nav-auth");
-        console.log("navAuth:", navAuth);
-        navAuth.classList.toggle("active");
-    });
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     const menuIcon = document.getElementById('menuIcon');
+//     const loginBtn = document.getElementById('loginBtn');
+//     const subscribeBtn = document.getElementById('subscribeBtn');
+
+//     menuIcon.addEventListener('click', function () {
+//         if (loginBtn.style.display === 'none' && subscribeBtn.style.display === 'none') {
+//             loginBtn.style.display = 'inline-block';
+//             subscribeBtn.style.display = 'inline-block';
+//             menuIcon.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+//         } else {
+//             loginBtn.style.display = 'none';
+//             subscribeBtn.style.display = 'none';
+//             menuIcon.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
+//         }
+//     });
+// });
+
+
+hamburger = document.querySelector('#menuIcon');
+navbar = document.querySelector('#navLinksContainer');
+
+hamburger.onclick = function () {
+    navbar.classList.toggle('active');
+
+    if (navbar.classList.contains('active')) {
+        hamburger.innerHTML = '<i class="fa fa-times" aria-hidden="true" width="24" hight="24"></i>';
+    } else {
+        hamburger.innerHTML = '<i class="fa fa-bars" aria-hidden="true" width="24" hight="24"></i>';
+    }
+};
+
